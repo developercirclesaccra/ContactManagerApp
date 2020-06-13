c44640f72cc34297d1b09b618536ea48ea952bff
Etornam Sunu <sunumacbright@gmail.com>
2020-06-13 10:52:23 +0000

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -49,59 +49,87 @@ class _HomePageState extends State<HomePage> {
                 return NewContactPage();
               }));
             },
+          ),
+          FlatButton(
+            child: Text('Logout',
+                style: Theme.of(context)
+                    .textTheme
+                    .subtitle2
+                    .copyWith(color: Colors.white)),
+            onPressed: () async {
+              await signOut().then((_) {
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                        (route) => false);
+              });
+            },
           )
         ],
       ),
       body: StreamBuilder<QuerySnapshot>(
-        stream: userDatabase.where('uploader_uid',isEqualTo: _userID).getDocuments().asStream(),
-        builder: (context, snapshot) {
-          if(!snapshot.hasData){
-            return CircularProgressIndicator();
-          }
-          print(snapshot.data.runtimeType);
-          return ListView.separated(
-              padding: EdgeInsets.all(16),
-              itemBuilder: (context, index) {
-                return Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Wrap(
-                      crossAxisAlignment: WrapCrossAlignment.center,
-                      children: [
-                        CircleAvatar(
-                          backgroundColor: Colors.blue,
-                          radius: 25,
-                          backgroundImage: NetworkImage('${snapshot.data.documents[index].data['picUrl']}'),
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          '${snapshot.data.documents[index].data['first_name']} ${snapshot.data.documents[index].data['last_name']}',
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyText1
-                              .copyWith(fontWeight: FontWeight.w600),
-                        ),
-                      ],
-                    ),
-                    Wrap(
-                      children: [
-                        IconButton(icon: Icon(Icons.call), onPressed: () {}),
-                        IconButton(icon: Icon(Icons.video_call), onPressed: () {}),
-                        IconButton(icon: Icon(Icons.message), onPressed: () {}),
-                      ],
-                    )
-                  ],
-                );
-              },
-              separatorBuilder: (context, index) {
-                return Divider();
-              },
-              itemCount: snapshot.data.documents.length);
-        }
-      ),
+          stream: userDatabase
+              .where('uploader_uid', isEqualTo: _userID)
+              .getDocuments()
+              .asStream(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: Text(
+                  'No Contact Added!',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline6
+                      .copyWith(fontWeight: FontWeight.w600),
+                ),
+              );
+            }
+
+            return ListView.separated(
+                padding: EdgeInsets.all(16),
+                itemBuilder: (context, index) {
+                  return Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Wrap(
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Colors.blue,
+                            radius: 25,
+                            backgroundImage: NetworkImage(
+                                '${snapshot.data.documents[index].data['picUrl']}'),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            '${snapshot.data.documents[index].data['first_name']} ${snapshot.data.documents[index].data['last_name']}',
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyText1
+                                .copyWith(fontWeight: FontWeight.w600),
+                          ),
+                        ],
+                      ),
+                      Wrap(
+                        children: [
+                          IconButton(icon: Icon(Icons.call), onPressed: () {}),
+                          IconButton(
+                              icon: Icon(Icons.video_call), onPressed: () {}),
+                          IconButton(
+                              icon: Icon(Icons.message), onPressed: () {}),
+                        ],
+                      )
+                    ],
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return Divider();
+                },
+                itemCount: snapshot.data.documents.length);
+          }),
     );
   }
 
