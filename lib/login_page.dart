6b48fda184914ec5749edc0481f79b932bc04f04
Etornam Sunu <sunumacbright@gmail.com>
2020-06-13 10:52:25 +0000

    Update login_page.dart

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:contactapp/home_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'functions.dart';
@@ -13,30 +14,51 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          Center(child: Text('login')),
-          Center(
-              child: _isLoading
-                  ? CircularProgressIndicator()
-                  : FlatButton(
-                      onPressed: () async {
-                        setState(() {
-                          _isLoading = true;
-                        });
-                        bool isAuth = await signInWithGoogle();
-                        setState(() {
-                          _isLoading = false;
-                        });
-                        if (isAuth) {
-                          Navigator.of(context).pushReplacement(
-                              MaterialPageRoute(builder: (context) {
-                            return HomePage();
-                          }));
-                        }
-                      },
-                      child: Text('Continue with Google')))
-        ],
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlutterLogo(
+              size: 120,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Center(child: Text('Login Page')),
+            SizedBox(height: 25),
+            Center(
+                child: _isLoading
+                    ? CircularProgressIndicator()
+                    : FlatButton(
+                        color: Colors.blue,
+                        onPressed: () async {
+                          setState(() {
+                            _isLoading = true;
+                          });
+                          bool isAuth = await signInWithGoogle();
+                          setState(() {
+                            _isLoading = false;
+                          });
+                          if (isAuth) {
+                            Navigator.pushAndRemoveUntil(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => HomePage()),
+                                (route) => false);
+                          }
+                        },
+                        child: Text(
+                          'Continue with Google',
+                          style: Theme.of(context)
+                              .textTheme
+                              .button
+                              .copyWith(color: Colors.white),
+                        )))
+          ],
+        ),
       ),
     );
   }
